686cc6c8f2213988f94fb0ca1b6b7de4ff643e44
StevenDelval <asteven59220@free.fr>
2022-12-21 17:48:26 +0100

    test

--- app.py
+++ app.py
@@ -14,15 +14,11 @@ fig, ax = plt.subplots()
 ax.hist(df_semaine,bins=30,color = "#45d4a1", ec="red",lw=2)
 
 
-
 st.pyplot(fig)
 
-
-df_semaine_type= pd.DataFrame(df[['date',"type_objet",'id']].groupby([pd.Grouper(key='date', freq='W'),"type_objet"]).count().reset_index().set_index("date"))
-mask_objet =st.multiselect("Choisissez un type d'objet",df_semaine_type["type_objet"].unique())
+df_semaine_type= pd.DataFrame(df[['date',"type_objet",'id']].groupby([pd.Grouper(key='date', freq='M'),"type_objet"]).count().reset_index().set_index("date"))
+df_semaine_type.rename(columns = {'id':'count_objet_mois'}, inplace = True)
+mask_objet =st.selectbox("Choisissez un type d'objet",df_semaine_type["type_objet"].unique())
 if st.button("Cree le graphique"):
-    combo_mask = df_semaine_type["type_objet"] == mask_objet[0]
-    for mask in mask_objet:
-        combo_mask |= df_semaine_type["type_objet"] == mask
-
-    st.line_chart(df_semaine_type[combo_mask]["id"])
+    
+    st.line_chart(df_semaine_type[df_semaine_type["type_objet"]==mask_objet]["count_objet_mois"])
