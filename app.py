506e503cd7e3cd176f2c7cd1162579e586d7f89b
StevenDelval <asteven59220@free.fr>
2022-12-22 11:42:36 +0100

    modif visualitation

--- app.py
+++ app.py
@@ -2,23 +2,65 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import sqlalchemy as db
+import folium
+import streamlit_folium
 
 engine = db.create_engine("sqlite:///db.sqlite")
 connection = engine.connect()
 
 df = pd.read_sql_table(table_name="LostItem",con=engine)
-df['date'] = pd.to_datetime(df['date']) - pd.to_timedelta(7, unit='d')
-df_semaine= pd.DataFrame(df.groupby([pd.Grouper(key='date', freq='W')])["type_objet"].count())
-
-fig, ax = plt.subplots()
-ax.hist(df_semaine,bins=30,color = "#45d4a1", ec="red",lw=2)
 
 
-st.pyplot(fig)
+sql=""" 
+SELECT * FROM LostItem
+JOIN Gare ON LostItem.code_uic_gare_origine = Gare.code_uic
+JOIN Frequentation on Frequentation.code_uic = LostItem.code_uic_gare_origine
 
-df_semaine_type= pd.DataFrame(df[['date',"type_objet",'id']].groupby([pd.Grouper(key='date', freq='M'),"type_objet"]).count().reset_index().set_index("date"))
-df_semaine_type.rename(columns = {'id':'count_objet_mois'}, inplace = True)
-mask_objet =st.selectbox("Choisissez un type d'objet",df_semaine_type["type_objet"].unique())
-if st.button("Cree le graphique"):
-    
-    st.line_chart(df_semaine_type[df_semaine_type["type_objet"]==mask_objet]["count_objet_mois"])
+"""
+df_join = pd.read_sql(sql,con=engine)
+
+
+tab1,tab2,tab3 = st.tabs(["Question n°1","Question n°2","Question n°3"])
+
+with tab1:
+
+
+    df['date'] = pd.to_datetime(df['date']) - pd.to_timedelta(7, unit='d')
+    df_semaine= pd.DataFrame(df.groupby([pd.Grouper(key='date', freq='W')])["type_objet"].count())
+
+    fig, ax = plt.subplots()
+    ax.hist(df_semaine,bins=30,color = "#45d4a1", ec="red",lw=2)
+
+
+    st.pyplot(fig)
+with tab2:
+    df_semaine_type= pd.DataFrame(df[['date',"type_objet",'id']].groupby([pd.Grouper(key='date', freq='M'),"type_objet"]).count().reset_index().set_index("date"))
+    df_semaine_type.rename(columns = {'id':'count_objet_mois'}, inplace = True)
+    mask_objet =st.selectbox("Choisissez un type d'objet",df_semaine_type["type_objet"].unique())
+    if st.button("Cree le graphique"):
+        
+        st.line_chart(df_semaine_type[df_semaine_type["type_objet"]==mask_objet]["count_objet_mois"])
+
+
+
+with tab3:
+    df_dep= pd.DataFrame(df_join[['code_departement','type_objet',"date",'departement']].groupby(['code_departement']).count().reset_index())
+
+
+    france = folium.Map(location = [46.763656, 2.429795], zoom_start = 5)
+
+
+
+    folium.Choropleth(
+        geo_data="departements.geojson",
+        name="France departements",
+        data=df_dep,
+        columns=["code_departement","type_objet"],
+        key_on="feature.properties.code",
+        fill_color="YlGn",
+        fill_opacity=0.7,
+        line_opacity=.1,
+        legend_name="Type d'objet (%)",
+    ).add_to(france)
+
+    streamlit_folium.st_folium(france)
